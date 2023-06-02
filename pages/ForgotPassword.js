1ab301835be4c58c80367d81bb5ed25e1063f7df
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-06-02 17:09:05 +0300

    Forgot password

---
+++ pages/ForgotPassword.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+  return (
+    <div className="container mx-auto py-20">
+      <div className="flex justify-center">
+        <div className="w-full max-w-md">
+          <div className="bg-gray-800 rounded-lg shadow-lg p-8">
+            <div className="text-center">
+              <a href="/" className="flex items-center justify-center mb-2">
+                <img src="/logo.png" alt="logo" className="h-8" />
+              </a>
+            </div>
+            <h4 className="text-2xl font-semibold mb-4">Forgot Password? 🔒</h4>
+            <p className="mb-8">Enter your email and we'll send you instructions to reset your password</p>
+            <form className="mb-6">
+              <div className="mb-4">
+                <label htmlFor="email" className="block mb-1 font-semibold">Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  autoFocus
+                />
+              </div>
+              <button className="w-full py-2 px-4 bg-blue-500 text-white rounded-full font-semibold hover:bg-blue-600" type="submit">Send Reset Link</button>
+            </form>
+            <div className="text-center">
+              <a href="auth-login-basic.html" className="text-blue-500">Back to login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ForgotPassword;
