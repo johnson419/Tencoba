8ab47328a6c00ec7f02068ee2a08c22348fc77a2
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-06-04 19:25:29 +0300

    Created foundation for sidebar

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,7 +1,15 @@
 import DashNav from "@/components/DashNav";
 
 const Dashboard = () => {
-  return <DashNav />;
+  return (
+    <div className="flex h-screen">
+      {/* <Sidebar /> */}
+      <div className="flex flex-col flex-1">
+        <DashNav />
+        {/* Main content of the dashboard */}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
