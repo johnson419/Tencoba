d8049758ba94f3c024e5be518afb57844f8a73bb
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-06-05 16:14:28 +0300

    rounded-xl sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,22 +4,30 @@ import { faHome, faChartLine, faWallet, faCreditCard } from '@fortawesome/free-s
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-800 text-white h-screen mx-auto top-0 left-3 py-4 px-2 flex flex-col items-center rounded-xl">
-      <a href="#" className="mb-8 flex flex-col items-center">
-        <FontAwesomeIcon icon={faHome} size="2x" className="mb-2" />
-        <span className="text-sm text-center">Home</span>
+    <div className="bg-gray-800 text-white h-screen mx-auto top-0 left-3 py-4 px-2 flex flex-col items-start rounded-xl">
+      <a href="#" className="mb-8 flex items-center">
+        <div className="w-6">
+          <FontAwesomeIcon icon={faHome} size="1x" />
+        </div>
+        <span className="ml-2 text-sm text-center">Home</span>
       </a>
-      <a href="#" className="mb-8 flex flex-col items-center">
-        <FontAwesomeIcon icon={faChartLine} size="2x" className="mb-2" />
-        <span className="text-sm text-center">Charts</span>
+      <a href="#" className="mb-8 flex items-center">
+        <div className="w-6">
+          <FontAwesomeIcon icon={faChartLine} size="1x" />
+        </div>
+        <span className="ml-2 text-sm text-center">Charts</span>
       </a>
-      <a href="#" className="mb-8 flex flex-col items-center">
-        <FontAwesomeIcon icon={faWallet} size="2x" className="mb-2" />
-        <span className="text-sm text-center">Wallet</span>
+      <a href="#" className="mb-8 flex items-center">
+        <div className="w-6">
+          <FontAwesomeIcon icon={faWallet} size="1x" />
+        </div>
+        <span className="ml-2 text-sm text-center">Wallet</span>
       </a>
-      <a href="#" className="mb-8 flex flex-col items-center">
-        <FontAwesomeIcon icon={faCreditCard} size="2x" className="mb-2" />
-        <span className="text-sm text-center">Credit Card</span>
+      <a href="#" className="mb-8 flex items-center">
+        <div className="w-6">
+          <FontAwesomeIcon icon={faCreditCard} size="1x" />
+        </div>
+        <span className="ml-2 text-sm text-center">Credit Card</span>
       </a>
     </div>
   );
