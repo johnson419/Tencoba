95d98714b7ec02a198be51c176bf6957eede2cc4
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-06-05 15:59:40 +0300

    rounded-xl

--- components/DashNav.js
+++ components/DashNav.js
@@ -25,7 +25,7 @@ const DashNav = () => {
   }, []);
 
   return (
-    <nav className="bg-gray-800 py-4">
+    <nav className="bg-gray-800 py-4 rounded-xl">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center">
           <div className="flex items-center">
