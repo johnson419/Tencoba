2426ca65aa61beac5c1ce1246f2decfb2ef35d4b
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-06-16 18:11:26 +0300

    side items round 1

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,8 +6,7 @@ import {
   faWallet,
   faCreditCard,
 } from "@fortawesome/free-solid-svg-icons";
-import { MdOutlineSpaceDashboard,
-MdOutlineAnalytics, } from "react-icons/md";
+import { MdOutlineSpaceDashboard, MdOutlineAnalytics, MdOutlineIntegrationInstructions } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { FaRegComments } from "react-icons/fa";
 import { BiMessageSquareDots } from "react-icons/bi";
@@ -28,14 +27,53 @@ const Sidebar = () => {
         {/* Side Panel */}
         <div className="p-6 w-1/2 h-screen bg-gray-800 z-20 fixed top-0 -left-96 lg:w-60 lg:left-0 peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
           <div className="flex flex-col justify-start items-center">
-                <h1 className="text-base text-center cursor-pointer font-bold text-gray-300 border-gray-100 pb-4 w-full">TENCOBA</h1>
-                <div className="my-4 border-b border-gray-800 pb-4">
-                  <div className="flex mb-2 justify-start items-center gap-5 pl-5 hover:bg-gray-500 p-2 rounded-md group cursor pointer hover:shadow-lg m-auto">
-                    {/* Needed Icons */}
-                    <MdOutlineSpaceDashboard className="text-2xl text-gray-300 group-hover:text-black" />
-                    <h3 className="text-base text-gray-300 group-hover:text-black font-semibold mr-2">Dashboard</h3>
-                  </div>
-                </div>
+            <h1 className="text-base text-center cursor-pointer font-bold text-gray-300 border-gray-100 pb-4 w-full">
+              TENCOBA
+            </h1>
+            <div className="my-4 border-b border-gray-800 pb-4">
+              <div className="flex mb-2 justify-start items-center gap-5 px-5 hover:bg-gray-500 p-2 rounded-md group cursor pointer hover:shadow-lg m-auto">
+                {/* Needed Icons */}
+                <MdOutlineSpaceDashboard className="text-2xl text-gray-300 group-hover:text-black" />
+                <h3 className="text-base text-gray-300 group-hover:text-black font-semibold mr-1">
+                  Dashboard
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-5 px-5 hover:bg-gray-500 p-2 rounded-md group cursor pointer hover:shadow-lg m-auto">
+                {/* Needed Icons */}
+                <CgProfile className="text-2xl text-gray-300 group-hover:text-black" />
+                <h3 className="text-base text-gray-300 group-hover:text-black font-semibold mr-1">
+                  Profile
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-5 px-5 hover:bg-gray-500 p-2 rounded-md group cursor pointer hover:shadow-lg m-auto">
+                {/* Needed Icons */}
+                <FaRegComments className="text-2xl text-gray-300 group-hover:text-black" />
+                <h3 className="text-base text-gray-300 group-hover:text-black font-semibold mr-1">
+                  Comments
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-5 px-5 hover:bg-gray-500 p-2 rounded-md group cursor pointer hover:shadow-lg m-auto">
+                {/* Needed Icons */}
+                <MdOutlineAnalytics className="text-2xl text-gray-300 group-hover:text-black" />
+                <h3 className="text-base text-gray-300 group-hover:text-black font-semibold mr-1">
+                  Analytics
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-5 px-5 hover:bg-gray-500 p-2 rounded-md group cursor pointer hover:shadow-lg m-auto">
+                {/* Needed Icons */}
+                <BiMessageSquareDots className="text-2xl text-gray-300 group-hover:text-black" />
+                <h3 className="text-base text-gray-300 group-hover:text-black font-semibold mr-1">
+                  Messages
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-5 px-5 hover:bg-gray-500 p-2 rounded-md group cursor pointer hover:shadow-lg m-auto">
+                {/* Needed Icons */}
+                <MdOutlineIntegrationInstructions className="text-2xl text-gray-300 group-hover:text-black" />
+                <h3 className="text-base text-gray-300 group-hover:text-black font-semibold mr-1">
+                  Integrations
+                </h3>
+              </div>
+            </div>
           </div>
         </div>
       </Disclosure>
